58aaef00dd5846e38516427004e668f35c2ec0a4
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-12 10:45:38 +0100

    commit - java doc

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -2,6 +2,11 @@ package com.epsi.mycal;
 
 public class Application {
 
+  /**
+   * ceci est le point d'entrée du programme
+   * @param args tab avec des arguments
+   */
+  
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("HelloWord");
