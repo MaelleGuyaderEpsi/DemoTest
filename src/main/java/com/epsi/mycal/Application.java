0912204f685d5c7966c52090c991e0c89940fa17
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-12 10:55:47 +0100

    commit - peut être ok?

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -1,8 +1,8 @@
 package com.epsi.mycal;
 
 public class Application {
-  /** ceci est le point d'entrée du programme
-   * @param args tab avec des arguments
+  /** ceci est le point d'entrée du programme.
+   * @param args tab avec des arguments.
    */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
