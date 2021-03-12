9ab61d680dce58404258b2b7326e207a03c7150a
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-12 10:32:04 +0100

    commit - plus de problème space

--- src/main/java/com/epsi/mycal/Calculator.java
+++ src/main/java/com/epsi/mycal/Calculator.java
@@ -1,17 +1,17 @@
 package com.epsi.mycal;
 
 public class Calculator {
-	
-	public int add(int a, int b) {
-		return a + b;
-	}
-	
-	public int sub(int a, int b) {
-		return a - b;
-	}
-	
-	public int mul(int a, int b) {
-		return a * b;
-	}
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int sub(int a, int b) {
+    return a - b;
+  }
+
+  public int mul(int a, int b) {
+    return a * b;
+  }
 
 }
