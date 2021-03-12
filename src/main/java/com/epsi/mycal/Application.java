92d78d6d3f02a2457929e78289dc99b49525cc76
DESKTOP-VDT1J7R\maell <maelle.guyader@epsi.fr>
2021-03-12 10:36:55 +0100

    commit - app

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -3,8 +3,9 @@ package com.epsi.mycal;
 public class Application {
 
   public static void main(String[] args) {
-    System.out.println("HelloWord");
     // TODO Auto-generated method stub
+    System.out.println("HelloWord");
+    
 
   }
 }
